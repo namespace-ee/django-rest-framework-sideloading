fe6ebe1f2be8cad7cdd80f94adce213c2fd2d2c3
demonno <demur.nodia@gmail.com>
2017-07-28 12:41:12 +0300

    Fix browsable api case

--- drf_sideloading/serializers.py
+++ drf_sideloading/serializers.py
@@ -5,6 +5,10 @@ class SideLoadableSerializer(serializers.Serializer):
     def __init__(self, *args, **kwargs):
         super(SideLoadableSerializer, self).__init__(*args, **kwargs)
 
+        # fix for drf browsable api
+        # https://github.com/encode/django-rest-framework/blob/master/rest_framework/renderers.py#L530
+        self.many = True
+
         for relation_name in args[0].keys():
             self.fields[relation_name] = kwargs['context']['view'].sideloadable_relations[relation_name](many=True,
                                                                                                          read_only=True)
