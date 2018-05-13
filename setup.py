10e169d0d9e77815ec873adf04fb149f10d56beb
Demur Nodia <demur.nodia@gmail.com>
2018-05-13 14:24:09 +0300

    Add lint and doc builds for travis (#29)
    * Add lint and doc builds for Travis
    
    * Add dependencies in _dev req file
    
    * Include docs and lint in Travis matrix

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "Django>=1.8,<1.12",
-        "djangorestframework>=3.1.0,<4"
+        "djangorestframework>=3.1.0"
     ],
     license="MIT",
     zip_safe=False,
