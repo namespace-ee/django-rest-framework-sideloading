ac600ca2ddddd06481069b17deafdbb94af689e6
Tõnis Väin <tonis@namespace.ee>
2018-08-22 17:11:24 +0300

    test for django 2.1

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,6 @@ from distutils.version import LooseVersion
 
 import django
 from django.apps import AppConfig
-from django.core.management import call_command
 
 DJANGO_20 = LooseVersion(django.__version__) >= LooseVersion('2.0')
 
@@ -10,7 +9,3 @@ DJANGO_20 = LooseVersion(django.__version__) >= LooseVersion('2.0')
 class TestsConfig(AppConfig):
     name = 'tests'
     verbose_name = "Tests"
-
-    def ready(self):
-        # apply migrations
-        call_command('migrate')
